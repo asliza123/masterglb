0ac53ed8662f03ab7a0778e77ea2187f546787fb
asus <asus@LAPTOP-HK8QK20C>
2022-08-24 15:13:24 +0530

    updated

---
+++ src/test/java/master/admin/DemoPracticeTest.java
@@ -0,0 +1,11 @@
+package master.admin;
+
+import org.testng.annotations.Test;
+
+public class DemoPractice {
+	@Test
+	public void practiceDemo() {
+		System.out.println("!!!All Is Well!!!");
+	}
+
+}
