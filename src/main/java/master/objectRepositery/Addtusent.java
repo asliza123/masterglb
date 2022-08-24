3b92e20b2da887127ff7c974bf49e095d30d198c
User <User@User-PC>
2022-08-24 11:47:53 +0530

    master demo

---
+++ src/main/java/master/objectRepositery/Addtusent.java
@@ -0,0 +1,17 @@
+package master.objectRepositery;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Addtusent {
+
+	public Addtusent(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(xpath = "//a[text()=' Add Student']")
+	private WebElement addstudent;
+	
+	
+}
